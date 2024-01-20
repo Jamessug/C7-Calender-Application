249a28fbcd400c0576fc1032b64697683c7a0374
Jamessug <jamessug@icloud.com>
2024-01-20 00:15:14 +0000

    v2 - created currentday variable with new date object. console logging to test

--- starter/logic.js
+++ starter/logic.js
@@ -1,3 +1,6 @@
 // logic
 
-console.log("hi");
+// Get the current date and store it as a string in the 'currentDay' variable
+const currentDay = new Date().toDateString();
+
+console.log(currentDay);
