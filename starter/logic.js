f37feb4387c4b839b3cc2a4457811424ee5c2e45
Jamessug <jamessug@icloud.com>
2024-01-22 22:34:18 +0000

    v2.1 - trying to show current day on the web page using getElelementByID

--- starter/logic.js
+++ starter/logic.js
@@ -3,4 +3,7 @@
 // Get the current date and store it as a string in the 'currentDay' variable
 const currentDay = new Date().toDateString();
 
+// locates HTML element with the ID "currentDay" and updates its visible text content
+document.getElementById("currentDay").textContent = currentDay;
+
 console.log(currentDay);
